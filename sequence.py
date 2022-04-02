d3703968ca713719e514d8423bb980aebf55eecd
Abdul Malik Jalloh | abdulmalikj.eth <abdulmalikj@aol.com>
2022-04-02 22:40:48 +0100

    Session 1: Intro to python epsiode 1

---
+++ sequence.py
@@ -0,0 +1,7 @@
+"1. Sequence : in python is the execution of code in the order in which it was written"
+# comments are written in single quotes '', double quotes "" or hash tag #. in fact this
+# line is commented out with #. Therefore, the python interpreter would not print it out 
+# unlike the two print statements below with their respective strings wrapped in quotes
+
+print("Welcome to the Python Diaries series")
+print("Season 1: Intro to python epsiode 1")
